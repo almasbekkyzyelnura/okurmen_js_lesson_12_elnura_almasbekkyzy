575f1704e7b1d6dd24d963f28441c3b61599dc94
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-03-13 11:03:53 +0600

    Added jquery kod

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
     function createElementToContainer(text , className) {
     const container = document.getElementById('container');
    // const container = document.querySelector('#container');
@@ -13,4 +13,31 @@ createElementToContainer('I am blueviolet ', 'container-child-1');
 createElementToContainer('I am red ', 'container-child-2');
 createElementToContainer('I am green', 'container-child-3');
 
+});    */
+
+
+
+
+
+
+
+$(function () {
+    function createElementToContainer(text, className) {
+        const container = $('#container');
+        const newElement = $(`<div class="${className}">${text}</div>`);
+        container.append(newElement);
+    }
+   // createElementToContainer('I am blueviolet ', 'container-child-1');
+   // createElementToContainer('I am red ', 'container-child-2');
+   // createElementToContainer('I am green', 'container-child-3');
+
+
+    const Listitem = $('.List-item');
+
+    Listitem.each(function (index) {
+        $(this).text(`Item ${index}`)
+    });
+const input = $('input[type="text"]');
+
+console.log(input.val('l love JS'));
 });
